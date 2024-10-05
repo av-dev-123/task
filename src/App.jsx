9d431e49758d7dc6f45f0c73fdb7102effe17765
akshay_vindra <akshay.megroup@gmail.com>
2024-10-05 15:39:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+function App() {
+  return (
+   <>
+    <Header/>
+    <main>
+      <div className="">
+        todo app
+      </div>
+    </main>
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+const Header =()=>{
+    return(
+        <>
+        <div className="text-red-600"> 
+        header
+        </div>
+        </>
+    )
+}
+export default Header
